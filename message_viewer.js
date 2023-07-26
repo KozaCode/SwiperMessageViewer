dac654463891046f6bd8a23b12b26ce57f512345
Sebastian Koza <kozasebastian45@gmail.com>
2023-07-26 20:35:39 +0200

    Added swiping buttons, added swiping by clicking

--- message_viewer.js
+++ message_viewer.js
@@ -1,149 +1,271 @@
 // ==UserScript==
-// @name         SwiperMessageViewer
+// @name         Listing content script
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  Read messages from the swiper and print them on the list on a created window
-// @author       KozaCode
-// @match        https://beta.character.ai/chat2*
+// @version      0.2
+// @description  Read messages from the swiper and print as a list on a created window
+// @author       You
+// @match        https://beta.character.ai/chat*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=character.ai
+// @grant        none
 // ==/UserScript==
 
 
 (function() {
     'use strict';
+
+    let messages = [];
+    let isScrolledToBottom = true;
+    let added = false;
+    let detected = false;
+    let activeMessageId = 0
+
+    let style = document.createElement('style');
+    style.setAttribute('type', 'text/css');
+    style.innerHTML = `
+    #window{
+        position: fixed;
+        top: 0;
+        right: 0;
+        width: 300px;
+        height: 100%;
+        background-color: black;
+        z-index: 9999;
+    }
+    #window h1{
+        color: white;
+        text-align: center;
+        margin-top: 10px;
+        margin-bottom: 10px;
+    }
+    #window button{
+        position: fixed;
+        right: 10px;
+        top: 40px;
+        background-color: transparent;
+        border: none;
+        color: white;
+        font-size: 20px;
+        cursor: pointer;
+        outline: none;
+        z-index: 10000;
+    }
+    #toggle{
+        position: fixed;
+        right: 10px;
+        top: 40px;
+        background-color: transparent;
+        border: none;
+        color: white;
+        font-size: 20px;
+        cursor: pointer;
+        outline: none;
+        z-index: 10000;
+    }
+    #list{
+        height: calc(100% - 100px);
+        overflow-y: scroll;
+        padding: 10px;
+        color: white;
+        font-size: 12px;
+        font-family: monospace;
+        line-height: 1.5;
+        text-align: justify;
+    }
+    .item{
+        margin-bottom: 10px;
+        border-bottom: 1px solid white;
+        display: flex;
+        flex-direction: row;
+    }
+    .pointer{
+        display: inline-block;
+        width: 10px;
+        height: 100%;
+        margin-right: 10px;
+        text-align: center;
+    }
+    .message{
+        display: inline-block;
+        width: calc(100% - 20px);
+    }
+    #left-arrow{
+        position: absolute;
+        left: 10px;
+        bottom: 0;
+        background-color: transparent;
+        border: none;
+        color: white;
+        font-size: 20px;
+        cursor: pointer;
+        outline: none;
+        z-index: 10000;
+    }
+    #right-arrow{
+        position: absolute;
+        right: 10px;
+        bottom: 0;
+        background-color: transparent;
+        border: none;
+        color: white;
+        font-size: 20px;
+        cursor: pointer;
+        outline: none;
+        z-index: 10000;
+    }
+
+    `;
+    document.getElementsByTagName('head')[0].appendChild(style);
+
+
     function createWindow(){
         const window = document.createElement('div');
-        window.style.position = 'fixed';
-        window.style.top = '0';
-        window.style.right = '0';
-        window.style.width = '300px';
-        window.style.height = '100%';
-        window.style.backgroundColor = 'black';
-        window.style.zIndex = '9999'
+        window.style.display = 'none';
+        window.id = 'window';
 
         const title = document.createElement('h1');
         title.innerText = 'Messages';
-        title.style.color = 'white';
-        title.style.textAlign = 'center';
-        title.style.marginTop = '10px';
-        title.style.marginBottom = '10px';
         window.appendChild(title);
 
         const toggle = document.createElement('button');
+        toggle.id = 'toggle';
         toggle.innerText = '⇅';
-        toggle.style.position = 'fixed';
-        toggle.style.right = '10px';
-        toggle.style.top = '40px';
-        toggle.style.backgroundColor = 'transparent';
-        toggle.style.border = 'none';
-        toggle.style.color = 'white';
-        toggle.style.fontSize = '20px';
-        toggle.style.cursor = 'pointer';
-        toggle.style.outline = 'none';
-        toggle.style.zIndex = '10000';
-        document.body.appendChild(toggle);
-
-
-        const list = document.createElement('div');
-        list.id = 'message-list';
-        list.style.height = 'calc(100% - 100px)';
-        list.style.overflowY = 'scroll';
-        list.style.padding = '10px';
-        list.style.color = 'white';
-        list.style.fontSize = '12px';
-        list.style.fontFamily = 'monospace';
-        list.style.lineHeight = '1.5';
-        list.style.textAlign = 'justify';
-
-        list.addEventListener('scroll', () => {
-            isScrolledToBottom = list.scrollHeight - list.scrollTop <= list.clientHeight;
-        });
-
-        window.appendChild(list);
-
         toggle.addEventListener('click', () => {
             if(window.style.display === 'none'){
+                console.log('show');
                 window.style.display = 'block';
 
             }else{
+                console.log('hide');
                 window.style.display = 'none';
 
             }
         });
+        document.body.appendChild(toggle);
+
+        const list = document.createElement('div');
+        list.id = 'list';
+
+        list.addEventListener('scroll', () => {
+            isScrolledToBottom = list.scrollHeight - list.scrollTop <= list.clientHeight;
+        });
+        window.appendChild(list);
+
+        const leftArrow = document.createElement('div');
+        leftArrow.id = 'left-arrow';
+        leftArrow.innerText = '←';
+        leftArrow.addEventListener('click', () => {
+            let button = document.querySelector('.swiper-button-prev');
+            button.click();
+        });
+        window.appendChild(leftArrow);
+
+        const rightArrow = document.createElement('div');
+        rightArrow.id = 'right-arrow';
+        rightArrow.innerText = '→';
+        rightArrow.addEventListener('click', () => {
+            let button = document.querySelector('.swiper-button-next');
+            button.click();
+        });
+        window.appendChild(rightArrow);
+
+
         document.body.appendChild(window);
     }
 
 
-    var messages = {};
-    var isScrolledToBottom = true;
 
     function updateList(){
         const wrapper = document.querySelector('.swiper-wrapper');
         const nodes = wrapper.querySelectorAll('.swiper-slide');
-        console.log(nodes.length)
-        var active = false
+        messages = [];
+        let id = 0;
         for(const node of nodes){
-            active = false;
-            if(node.classList.contains('swiper-slide-active') === true){
-                active = true;
-            }
-            if(node.querySelector('p') === null){
-                messages.push({message: " ", active: active});
-            }else if(node.querySelector('span.typing-dot') !== null){
-                messages.push({message: "...", active: active});
-            }else{
-                messages.push({message: node.querySelector('p').innerHTML, active: active});
-            }
+            let message = node.querySelector('span.typing-dot') !== null ? '...' : node.querySelector('p')?.innerHTML || ' ';
+            let active = node.classList.contains('swiper-slide-active');
+
+            messages.push({id: id, message: message, active: active});
+            id++;
         }
     }
-    function displayMessages(){
-        console.log("Displaying messages")
-        const list = document.getElementById('message-list');
-        list.innerHTML = '';
-        console.log(messages.length)
-        messages.forEach((message) => {
+
+    function createMessageElements(){
+        return messages.map(message => {
             const item = document.createElement('div');
-            item.style.marginBottom = '10px';
-            item.style.borderBottom = '1px solid white';
-            if(message.message === " "){
-                item.innerText = "-";
-            }else{
-                item.innerHTML = message.message;
+            item.classList.add('item');
+            item.dataset.id = message.id;
 
-            }
+            item.addEventListener('click', () => {
+                isScrolledToBottom = false;
+                let id = item.dataset.id;
+                let diff = id - activeMessageId;
+                console.log("dif = " + diff);
+                if(diff > 0){
+                    let button = document.querySelector('#right-arrow');
+                    for(let i = 0; i < diff; i++){
+                        setTimeout(() => {
+                            button.click();
+                        }, 250);
+                    }
+                }else if(diff < 0){
+                    console.log("BACK");
+                    let button = document.querySelector('#left-arrow');
+                    for(let i = diff; i<0; i++){
+                        setTimeout(() => {
+                            button.click();
+                        }, 250);
+                    }
+                }
+            });
+
+
+
+            const pointer = document.createElement('div');
+            pointer.classList.add('pointer');
+            pointer.innerHTML = message.active ? '→' : ' ';
+            item.appendChild(pointer);
+
+            const messageElement = document.createElement('div');
+            messageElement.classList.add('message');
+            messageElement.innerHTML = message.message === " " ? "-" : message.message;
+            messageElement.style.color = message.active ? 'green' : 'white';
             if(message.active){
-                item.style.color = 'green';
+                activeMessageId = message.id;
             }
+            item.appendChild(messageElement);
 
-            list.appendChild(item);
+
+
+            return item;
         });
+    }
+
+    function displayMessages(){
+        const list = document.getElementById('list');
+        list.innerHTML = '';
+        const messages = createMessageElements();
+        messages.forEach((message) => list.appendChild(message));
         if(isScrolledToBottom){
             list.scrollTop = list.scrollHeight;
         }
     }
 
-    var added = false;
-    var detected = false;
     const observer = new MutationObserver((mutations) => {
         added = false;
         for (const mutation of mutations) {
-            if(mutation.type !== 'childList') continue;
-            if(mutation.addedNodes.length === 0) continue;
-            if(mutation.addedNodes[0].localName !== 'p') continue;
-            if(detected === false){
-                messages = [];
-                detected = true;
+            if(mutation.type === 'childList' && mutation.addedNodes.length > 0 && mutation.addedNodes[0].localName === 'p'){
+                added = true;
+                break;
             }
-            added = true;
-            break;
         }
         if(added){
+            observer.disconnect();
             updateList();
             displayMessages();
-            detected = false;
+            observer.observe(document.body, { childList: true, subtree: true });
         }
 
     });
+
     observer.observe(document.body, { childList: true, subtree: true });
     createWindow();
 })();
